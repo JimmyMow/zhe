2fae2df28b999a1c04cbc234b6011d07e2f9153d
Jack Mallers <jimmymowschess@gmail.com>
2016-04-12 01:40:22 -0500

    changed it from o_game to game

--- app/views/wager.py
+++ app/views/wager.py
@@ -15,6 +15,6 @@ def all_wagers():
 @wagerbp.route('/<path:wager_id>', methods=['GET'])
 def wager(wager_id):
     wager = models.MLBWager.query.filter_by(id=wager_id).first()
-    game = mlb.get_game(wager.game_id)
-    o_game = mlb.get_mlb_game(wager.game_id)
-    return render_template('wager/show.html', wager=wager, game=game, o_game=o_game)
+    # game = mlb.get_game(wager.game_id)
+    game = mlb.get_mlb_game(wager.game_id)
+    return render_template('wager/show.html', wager=wager, game=game)
