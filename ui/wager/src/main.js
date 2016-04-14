dfbc8a3ab5d15231984599c6893fed173ea88e2c
Jack Mallers <jimmymowschess@gmail.com>
2016-04-14 00:24:25 -0500

    completed first design of mlb wager show

--- ui/wager/src/main.js
+++ ui/wager/src/main.js
@@ -1,4 +1,5 @@
 var m = require('mithril');
+var extra = require('./extra');
 
 var ge = document.getElementById('game_events');
 
@@ -32,12 +33,23 @@ function buildEvents(data) {
       try {
          var away = inning.top.atbat.map(function(atbat) {
             if ( atbat['@score'] ) {
+               console.log(inning);
                return m('div', { class: ['score-conatiner', 'cf'].join(' ') },
                m('div', { class: ['score', 'away'].join(' ') },
-                  m('div', {class: 'details'},
-                     m('p', { class: 'description' }, atbat['@des'])
-                  ),
-                  m('img', {src: 'http://mlb.mlb.com/images/players/mugshot/ph_' + atbat['@batter'] + '.jpg'})
+                  m('div', { class: 'inning' }, "Top of " + extra.ordinal_suffix_of(parseInt(inning['@num']))),
+                  m('div', { class: 'score_content' },
+                     m('img', {src: 'http://mlb.mlb.com/images/players/mugshot/ph_' + atbat['@batter'] + '.jpg'}),
+                     m('div', {class: 'details'},
+                        m('p', { class: 'description' }, atbat['@des']),
+                        m('span', { class: ['badge', 'event'].join(' ') }, atbat['@event']),
+                        m('span', { class: ['badge', 'score_total'].join(' ') },
+                           m('span', {class: 'away_mini_logo'}),
+                           m('span', {class: 'away_team_runs'}, atbat['@away_team_runs']),
+                           m('span', {class: 'home_mini_logo'}),
+                           m('span', {class: 'home_team_runs'}, atbat['@home_team_runs'])
+                        )
+                     )
+                  )
                ));
             }
          });
@@ -51,11 +63,22 @@ function buildEvents(data) {
             if ( atbat['@score'] ) {
                return m('div', { class: ['score-conatiner', 'cf'].join(' ') },
                m('div', { class: ['score', 'home'].join(' ') },
-                  m('div', {class: 'details'},
-                     m('p', { class: 'description' }, atbat['@des'])
-                  ),
-                  m('img', {src: 'http://mlb.mlb.com/images/players/mugshot/ph_' + atbat['@batter'] + '.jpg'})
-               ));
+                  m('div', { class: 'inning' }, "Bottom of " + extra.ordinal_suffix_of(parseInt(inning['@num']))),
+                  m('div', { class: 'score_content' },
+                  m('img', {src: 'http://mlb.mlb.com/images/players/mugshot/ph_' + atbat['@batter'] + '.jpg'}),
+                     m('div', {class: 'details'},
+                        m('p', { class: 'description' }, atbat['@des']),
+                        m('span', { class: ['badge', 'event'].join(' ') }, atbat['@event']),
+                        m('span', { class: ['badge', 'score_total'].join(' ') },
+                           m('span', {class: 'away_mini_logo'}),
+                           m('span', {class: 'away_team_runs'}, atbat['@away_team_runs']),
+                           m('span', {class: 'home_mini_logo'}),
+                           m('span', {class: 'home_team_runs'}, atbat['@home_team_runs'])
+                        )
+                        )
+                     )
+                  )
+               );
             }
          });
       }
@@ -70,5 +93,3 @@ function buildEvents(data) {
 
    m.render(ge, html);
 }
-
-console.log("yo from wager_show");
