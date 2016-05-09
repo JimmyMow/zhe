31f62d2f2fbe714869c39b66e2a2d3c3deb7b6a0
Jack Mallers <jimmymowschess@gmail.com>
2016-05-09 16:41:19 -0500

    setup mithril profile

---
+++ ui/profile/gulpfile.js
@@ -0,0 +1,60 @@
+var source = require('vinyl-source-stream');
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var watchify = require('watchify');
+var browserify = require('browserify');
+var uglify = require('gulp-uglify');
+var streamify = require('gulp-streamify');
+
+var sources = ['./src/main.js'];
+var destination = '../../app/static/compiled/';
+
+var onError = function(error) {
+  gutil.log(gutil.colors.red(error.message));
+};
+
+var standalone = 'profile';
+
+gulp.task('prod', function() {
+  console.log("prod");
+  return browserify('./src/main.js', {
+    standalone: standalone
+  }).bundle()
+    .on('error', onError)
+    .pipe(source('profile.min.js'))
+    .pipe(streamify(uglify()))
+    .pipe(gulp.dest(destination));
+});
+
+gulp.task('dev', function() {
+  console.log("dev");
+  return browserify('./src/main.js', {
+    standalone: standalone
+  }).bundle()
+    .on('error', onError)
+    .pipe(source('profile.js'))
+    .pipe(gulp.dest(destination));
+});
+
+gulp.task('watch', function() {
+  console.log('here');
+  var opts = watchify.args;
+  opts.debug = true;
+  opts.standalone = standalone;
+
+  var bundleStream = watchify(browserify(sources, opts))
+    .on('update', rebundle)
+    .on('log', gutil.log);
+
+  function rebundle() {
+    console.log(destination);
+    return bundleStream.bundle()
+      .on('error', onError)
+      .pipe(source('profile.js'))
+      .pipe(gulp.dest(destination));
+  }
+
+  return rebundle();
+});
+
+gulp.task('default', ['watch', 'dev']);
